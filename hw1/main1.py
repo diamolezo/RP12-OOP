ba398717ed4b6c78bc5ec345f09743a26bf401e4
diamolezo <diamolezo@gmail.com>
2025-06-08 18:28:25 +0300

    ADD: hw1 and main.py

---
+++ hw1/main1.py
@@ -0,0 +1,12 @@
+class Animal:
+    def __init__(self, name: str, type: str, age: int):
+        self.name = name
+        self.type = type
+        self.age = age
+
+    def make_sounds(self, sound: str):
+        print(f"Animal {self.type} {self.name} make sounds '{sound}'")
+
+
+example = Animal(name="Bobik", type="dog", age="5")
+example.make_sounds("gav-gav")
